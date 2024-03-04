34e0943086a39014b72a475648f2b7f5c4193189
KauanF3rnandes <kauangabrielfp97@gmail.com>
2024-03-04 01:58:56 -0300

    creating routes

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Header.css'
+
+const Header = () => {
+
+    const navigate = useNavigate()
+
+    return (  
+        <header>
+            <nav>
+
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
