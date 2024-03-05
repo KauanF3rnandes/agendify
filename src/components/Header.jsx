fd95ffb8f5745dd50fc22368f4f4b7b7247690be
KauanF3rnandes <kauangabrielfp97@gmail.com>
2024-03-05 00:17:17 -0300

    updating header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Header.css'
 import { Box } from "@chakra-ui/react"
-import { Center, Text } from '@chakra-ui/layout';
+import { Center, Text, Link } from '@chakra-ui/layout';
+import { Button, ButtonGroup, Spacer } from '@chakra-ui/react'
 
 const Header = () => {
 
     const navigate = useNavigate()
 
     return (  
-        <header>
+        <header className="header">
+            <div className="background-image"></div>
             <Center>
-                <Box w="80%" h="60px" bg='gray.200' className='navBar'>
-                    <Text pl='7' as='b'>Agendify</Text>
+                <Box w="80%" h="60px" bg='gray.200' className='navBar' boxShadow='lg'>
+                    <Link pl='7' as='b' href='/' _hover={{textDecoration: 'none'}}>Agendify</Link>
+                    <Spacer/>
+                    <Button mr='7' colorScheme='teal'>Acessar</Button>
                 </Box>
             </Center>
         </header>
