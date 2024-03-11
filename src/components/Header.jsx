cc268971a6335e2e3427fe86c658b173ac6c8c70
KauanF3rnandes <kauangabrielfp97@gmail.com>
2024-03-11 00:13:34 -0300

    updating header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,21 +4,31 @@ import './Header.css'
 import { Box } from "@chakra-ui/react"
 import { Center, Text, Link } from '@chakra-ui/layout';
 import { Button, ButtonGroup, Spacer } from '@chakra-ui/react'
+import TypeIt from "typeit-react";
+
 
 const Header = () => {
 
-    const navigate = useNavigate()
+    const navigate = useNavigate()  
 
     return (  
-        <header className="header">
-            <div className="background-image"></div>
-            <Center>
-                <Box w="80%" h="60px" bg='gray.200' className='navBar' boxShadow='lg'>
-                    <Link pl='7' as='b' href='/' _hover={{textDecoration: 'none'}}>Agendify</Link>
-                    <Spacer/>
-                    <Button mr='7' colorScheme='teal'>Acessar</Button>
-                </Box>
-            </Center>
+        <header>
+            <Box className="background-image">
+                <Center>
+                    <Box w="80%" h="60px" bg='gray.200' className='navBar' boxShadow='lg'>
+                        <Link pl='7' as='b' href='/' _hover={{textDecoration: 'none'}}>Agendify</Link>
+                        <Spacer/>
+                        <Button mr='7' colorScheme='teal'>Acessar</Button>
+                    </Box>
+                </Center>
+                <Center>
+                    <Box mt='20'>
+                        <Text className='titulo'>Sistema de Agendamento / Reserva
+                            
+                        </Text>
+                    </Box>
+                </Center>
+            </Box>
         </header>
     );
 }
